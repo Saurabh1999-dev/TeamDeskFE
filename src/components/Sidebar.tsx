36c25f6f535abbb858622e6b9986c62775d83fd7
Saurabh <saurabh.kumardev23@gmail.com>
2025-08-05 12:36:11 +0530

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom";
-import logo from '../Images/logo.png'
+import logo from '../images/logo.png'
 
 const navItems = [
     { label: "Dashboard", path: "/" },
